ec7a1c03804f4e15a5f1cea06eb3fb6d619546b5
Mark Sellors <mark.sellors@gmail.com>
2024-03-28 23:38:22 +0000

    Adds repo sync script

---
+++ repo-sync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CURRENT_DIR=$(basename $PWD)
+
+if [ "$CURRENT_DIR" == "bullseye" ]; then
+    S3_PATH="s3://debs.r4pi.org/bullseye/"
+elif [ "$CURRENT_DIR" == "bookworm" ]; then
+    S3_PATH="s3://debs.r4pi.org/bookworm/"
+else
+    echo "Error: command must be run from within the repo directory"
+    exit 1
+fi
+
+# s3cmd sync --delete-removed ./ s3://debs.r4pi.org
+s3cmd sync --delete-removed ./ "$S3_PATH"
